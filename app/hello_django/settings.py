bd51adfc8dee35e746b4851ca5f95f6ead6ec164
Julia Przygoda <przygodajulia2@gmail.com>
2024-11-30 14:52:42 +0100

    Fixing settings.py

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xLjrUpIjFIveHEo-s3WqOCw2QyBgzXxBV_zAlS71lRt3wAWKTLaGHKv_bJb5UjgdRU8'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
